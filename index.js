c82eb78e05d4923e6c6e5fce3095cbda9c5fe716
rnktlhr <rinakitluhur10@gmail.com>
2025-10-20 09:45:56 +0700

    menambahkan Database connection

--- index.js
+++ index.js
@@ -14,4 +14,19 @@ app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
+// Database connection
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Luhur2004_',
+    database: 'mahasiswa',
+    port: 3308
+});
 
+db.connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MySQL database.');
+});
