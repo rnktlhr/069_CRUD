8d9274cd65923bc133cbdaa7b302b6e687a879ad
rnktlhr <rinakitluhur10@gmail.com>
2025-10-20 09:45:24 +0700

    menambahkan index js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+let mysql = require('mysql2');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
